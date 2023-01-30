577585b228dcada2bfd9a4575da320c047538c45
mac <mac@macs-MacBook-Pro.local>
2023-01-30 13:18:58 +0200

    edit text

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Abeer AbdelSamee',
+      title: 'Abeer AbdelSamee Mhd ',
       theme: ThemeData(
         // This is the theme of your application.
         //
